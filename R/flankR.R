ed245551961c4f9e19d7b7e73f946d61750d10b3
Tim <timothy.a.myers@gmail.com>
2024-04-08 09:25:43 -0400

    add up and down stream variables, minor notes

--- R/flankR.R
+++ R/flankR.R
@@ -11,17 +11,26 @@ BiocManager::install("biomaRt")
 
 library(biomaRt)
 
+# 1.1) Input list of query rsIDs
+# Simple
 snp_list <- c("rs3", "rs4")
 
-# Select database
+# 1.2) Input list of query rsIDs
+snp_list <- readLines("data/input/rcc_snps_39.txt")
+
+# 2) Input number of flanking sequences to retrieve
+up_stream <- 15
+down_stream <- 5
+
+# 3) Select database
 snp_mart<- useMart("ENSEMBL_MART_SNP", dataset = "hsapiens_snp")
 
-# Retrieve sequence and other data
+# 4) Retrieve sequence and other data
 snp_sequence <- getBM(attributes = c("refsnp_id", "snp", "allele", "chr_name", "chrom_start", 
                                      "chrom_end", "chrom_strand"), 
                       filters = c("snp_filter", "upstream_flank", "downstream_flank"), 
                       checkFilters = FALSE, 
-                      values = list(snp_list, 10, 10), 
+                      values = list(snp_list, up_stream, down_stream), 
                       mart = snp_mart, 
                       bmHeader = TRUE)
 
