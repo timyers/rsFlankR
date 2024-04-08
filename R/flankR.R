930810ade98baced36165ef7894a5e0a365c3854
Tim <timothy.a.myers@gmail.com>
2024-04-08 08:35:03 -0400

    Initial commit

---
+++ R/flankR.R
@@ -0,0 +1,27 @@
+# Retrieve flanking sequences of rsID, 20 bp upstream and downstream
+# Reference: https://support.bioconductor.org/p/89688/
+# Reference: https://chat.openai.com/share/7e2ec69e-a377-4564-a525-0c9e96647499
+# NOTE: dataset = "hsapiens_snp" uses GRCh38.p14, do 'listDatasets(snp_mart)
+
+# install biomaRt
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("biomaRt")
+
+library(biomaRt)
+
+snp_list <- c("rs3", "rs4")
+
+# Select database
+snp_mart<- useMart("ENSEMBL_MART_SNP", dataset = "hsapiens_snp")
+
+# Retrieve sequence and other data
+snp_sequence <- getBM(attributes = c("refsnp_id", "snp", "allele", "chrom_start", "chrom_end", "chrom_strand"), 
+                      filters = c("snp_filter", "upstream_flank", "downstream_flank"), 
+                      checkFilters = FALSE, 
+                      values = list(snp_list, 10, 10), 
+                      mart = snp_mart, 
+                      bmHeader = TRUE)
+
+snp_sequence
